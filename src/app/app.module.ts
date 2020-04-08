a93be431adc1128223e85a9e0b3d81edad341b23
github username <github email address>
2020-04-08 12:48:01 -0400

    fix components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms'
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponentComponent } from './header-component/header-component.component';
 import { ShoppingListComponentComponent } from './shopping-list/shopping-list-component/shopping-list-component.component';
 import { ShoppingListEditorCompnentComponent } from './shopping-list/shopping-list-editor-compnent/shopping-list-editor-compnent.component';
 import { RecipeListComponentComponent } from './recipe-book/recipe-list-component/recipe-list-component.component';
-import { RecipeItemComponentComponent } from './recipe-book/recipe-item-component/recipe-item-component.component';
 import { RecipeDetailComponentComponent } from './recipe-book/recipe-detail-component/recipe-detail-component.component';
+import { RecipesComponentComponent } from './recipe-book/recipes-component/recipes-component.component';
 
 @NgModule({
   declarations: [
@@ -17,8 +17,8 @@ import { RecipeDetailComponentComponent } from './recipe-book/recipe-detail-comp
     ShoppingListComponentComponent,
     ShoppingListEditorCompnentComponent,
     RecipeListComponentComponent,
-    RecipeItemComponentComponent,
-    RecipeDetailComponentComponent
+    RecipeDetailComponentComponent,
+    RecipesComponentComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/recipe-book/recipes-component/recipes-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipes-component',
+  templateUrl: './recipes-component.component.html',
+  styleUrls: ['./recipes-component.component.css']
+})
+export class RecipesComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
