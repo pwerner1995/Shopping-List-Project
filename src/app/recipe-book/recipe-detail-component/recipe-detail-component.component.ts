22d52e23c71df3b666ac0b1062093d36f056a7b8
github username <github email address>
2020-04-28 12:15:53 -0400

    finish adding routing and child routing

--- src/app/recipe-book/recipe-detail-component/recipe-detail-component.component.ts
+++ src/app/recipe-book/recipe-detail-component/recipe-detail-component.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-detail-component',
@@ -12,7 +12,7 @@ export class RecipeDetailComponentComponent implements OnInit {
   recipe: Recipe
   id: number
 
-  constructor(private recipeService: RecipeService, private route: ActivatedRoute) { }
+  constructor(private recipeService: RecipeService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
     this.route.params
@@ -26,6 +26,10 @@ export class RecipeDetailComponentComponent implements OnInit {
 
   onAddToShoppingList(){
     this.recipeService.addToShoppingList(this.recipe.ingredients)
+  }
+
+  onEditRecipe(){
+    this.router.navigate(['edit'], {relativeTo: this.route})
   }
 
 }
