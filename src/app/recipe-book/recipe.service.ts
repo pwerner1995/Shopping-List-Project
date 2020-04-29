b2868dd3348f86eaa1ebee46980e34a5c72b1b98
github username <github email address>
2020-04-29 14:45:38 -0400

    clean up observables - remove unnecessary EventEmitters

--- src/app/recipe-book/recipe.service.ts
+++ src/app/recipe-book/recipe.service.ts
@@ -1,5 +1,5 @@
 import {Recipe} from './recipe.model'
-import { EventEmitter, Injectable } from '@angular/core'
+import {  Injectable } from '@angular/core'
 import { Ingredient } from '../shared/ingredient.model'
 import { ShoppingListService } from '../shopping-list/shopping-list.service'
 
@@ -20,7 +20,6 @@ export class RecipeService{
         ])
       ]
 
-    recipeSelected = new EventEmitter <Recipe>()
 
     constructor(private slService: ShoppingListService){}
 
